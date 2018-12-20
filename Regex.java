e2cf780d9038a28b75da0a97a99c949dfc11dbcb
SahilButani <sahilbutani1760@gmail.com>
2018-12-20 10:48:19 +0530

    add files via upload

---
+++ Regex.java
@@ -0,0 +1,12 @@
+import java.util.regex.*;
+public class Regex{
+    public static void main(String args[]){
+
+        Pattern p = Pattern.compile("^[A-Z].$");//. represents single character
+        Matcher m = p.matcher("Sahil.");
+        boolean b = m.matches();
+
+
+
+        System.out.println(b);
+    }}  
