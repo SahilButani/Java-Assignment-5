70fa8cc910a04976aaeb2b73d6c5d12daa8f100d
Sahil Butani <sahilbutani1760@gmail.com>
2019-01-21 16:45:24 +0530

    Update Regex.java

--- Regex.java
+++ Regex.java
@@ -2,7 +2,7 @@ import java.util.regex.*;
 public class Regex{
     public static void main(String args[]){
 
-        Pattern p = Pattern.compile("^[A-Z].*[.]$");//. represents single character
+        Pattern p = Pattern.compile("^[A-Z].*[.]$");//. regex to check if a string begins with a capital letter and ends with a period.
         Matcher m = p.matcher("Sahil.");
         boolean b = m.matches();
 
